f493f0442820a8931fb357f7a5a6194f2c792b41
Katerina Zamiatina <eozamyatina@gmail.com>
2021-04-02 14:25:34 +0300

    update: put contacts into var

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Form from './components/Form';
 import ContactsList from './components/Contacts';
 import Filter from './components/Filter';
 
+const contactsTest = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(contactsTest);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
